063e584a91920562bbcd8eed9714feb2b30c51a4
Anja <rasoloanja@gmail.com>
2019-04-28 16:04:30 -0400

    Added markers

--- src/Containers/Map/Map.js
+++ src/Containers/Map/Map.js
@@ -7,17 +7,21 @@ const Map = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDRoF3AQcn0aoVUEHOUwoQnWlgf-m60GWs&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
+    containerElement: <div style={{ height: `400px`, margin: `50px`}} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,
   withGoogleMap
-)((props) =>
+) ((props) =>
+
   <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: 45.50884, lng: -73.58781 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: 45.50884, lng: -73.58781 }} onClick={props.onMarkerClick} />}
+    defaultZoom={12}
+    defaultCenter={{ lat: 45.50884, lng: -73.58781 }}>
+
+            <Marker position={{ lat: 45.50884, lng: -73.58781 }} isMarkerShown={props.isMarkerShown} onClick={props.onMarkerClick} />
+            <Marker position={{ lat: 45.538207, lng:  -73.637691 }} isMarkerShown={props.isMarkerShown} onClick={props.onMarkerClick} />
+            <Marker position={{ lat: 45.538207, lng:  -73.537691 }} isMarkerShown={props.isMarkerShown} onClick={props.onMarkerClick} />
+
   </GoogleMap>
 )
 
