dc07819b0841fe50daad1e1759ffc01cfc557b7f
Anja <rasoloanja@gmail.com>
2019-04-28 17:35:12 -0400

    Init redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Map from './Containers/Map/Map';
+import Map from './containers/Map/Map';
 
 
 class App extends Component {
---
+++ src/store/reducer.js
@@ -0,0 +1,17 @@
+import * as actionTypes from './actions';
+import {products} from './data';
+
+const initialState = {
+    products: products,
+}
+
+
+const reducer =(state = initialState, action) => {
+
+}
+
+
+export default reducer;
+
+
+
