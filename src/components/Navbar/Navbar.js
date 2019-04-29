33e7b9a3aa733ef031f51f2d36d951dca0a637ac
Anja <rasoloanja@gmail.com>
2019-04-29 14:07:18 -0400

    Added svg icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import classes from './Navbar.css';
 import { NavLink } from 'react-router-dom';
+import ReactSVG from 'react-svg';
+
+import phone from '../../assets/icon/SVG/phone.svg';
+import mail from '../../assets/icon/SVG/envelop.svg';
+import user from '../../assets/icon/SVG/user.svg';
+import pencil from '../../assets/icon/SVG/pencil.svg';
+import facebook from '../../assets/icon/SVG/facebook.svg';
+import twitter from '../../assets/icon/SVG/twitter.svg';
+import linkedin from '../../assets/icon/SVG/linkedin2.svg';
+import google from '../../assets/icon/SVG/google-plus.svg';
 
 const navbar = () => {
   return (
