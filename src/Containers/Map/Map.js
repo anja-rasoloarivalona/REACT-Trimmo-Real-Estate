965b0245aac693d2bff79ca35f5e581bf0bf3f00
Anja <rasoloanja@gmail.com>
2019-04-29 10:27:48 -0400

    Added markers dynamically

--- src/Containers/Map/Map.js
+++ src/Containers/Map/Map.js
@@ -9,25 +9,42 @@ class Map extends Component {
   };
 
   renderMap = () => {
-    this.loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDRoF3AQcn0aoVUEHOUwoQnWlgf-m60GWs&callback=initMap")
-    window.initMap = this.initMap;
+      this.loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDRoF3AQcn0aoVUEHOUwoQnWlgf-m60GWs&callback=initMap")
+      window.initMap = this.initMap;
   }
 
   initMap = () => {
-   const map = new window.google.maps.Map(document.getElementById('map'), {
-     center: {lat: -34.389, lng: 150.644},
-     zoom: 12
-   })
-  }
+   const map = new window.google.maps.Map(document.getElementById('map'),
+        {
+            center: {lat: 45.50884, lng: -73.58781},
+            zoom: 12
+        });
+
+        let markerInfos = [...this.props.products];
+
+        markerInfos.map(item => {      
+          return (
+            new window.google.maps.Marker({
+              position: {lat: item.lat, lng: item.lng},
+              map: map
+              })
+          )
+
+        })
+
+        
+
+
+    }
 
     loadScript = (url) => {
-    let index = window.document.getElementsByTagName("script")[0];
-    let script = window.document.createElement("script");
-    script.src = url
-    script.async = true;
-    script.defer = true;
-    index.parentNode.insertBefore(script, index);
-  }
+        let index = window.document.getElementsByTagName("script")[0];
+        let script = window.document.createElement("script");
+        script.src = url
+        script.async = true;
+        script.defer = true;
+        index.parentNode.insertBefore(script, index);
+    }
 
 
   render() {
