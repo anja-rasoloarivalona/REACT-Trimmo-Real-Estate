24dd8951d7af85e1ef1286432451b90c58af80b0
Anja <rasoloanja@gmail.com>
2019-04-29 16:40:59 -0400

    Added background to home section

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import classes from './Home.css';
 
 import Form from './form/Form';
+import bg from '../../assets/img/bg.jpg';
+
+
+
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <Form />
-      </div> 
+      <section
+          style={{backgroundImage: "url(" + bg + ")",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover"}}
+          className={classes.Home}>
+          <Form />
+      </section> 
     
     )
   }
