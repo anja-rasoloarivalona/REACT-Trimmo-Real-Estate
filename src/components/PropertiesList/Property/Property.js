5cc8f7d5d352a21764df0da7ad44275afe1381bf
Anja <rasoloanja@gmail.com>
2019-04-29 17:50:41 -0400

    Added icon to property

--- src/components/PropertiesList/Property/Property.js
+++ src/components/PropertiesList/Property/Property.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import classes from './Property.css';
+import ReactSVG from 'react-svg';
+
+
 import bg from '../../../assets/img/bg.jpg';
+
+import heart from '../../../assets/icon/SVG/heart.svg';
+import home from '../../../assets/icon/SVG/home.svg';
+import garage from '../../../assets/icon/SVG/automobile.svg';
+import bed from '../../../assets/icon/SVG/location-hotel.svg';
+import bath from '../../../assets/icon/SVG/shower.svg';
+import location from '../../../assets/icon/SVG/location.svg';
+
 
 const property = () => {
   return (
@@ -10,6 +21,7 @@ const property = () => {
             style={{backgroundImage: "url(" + bg + ")",
             backgroundPosition: "center",
             backgroundSize: "cover"}}>
+            <ReactSVG src={heart} className={classes.Icon}/>
         </div>
 
         <div className={classes.TitleContainer}>
@@ -17,14 +29,32 @@ const property = () => {
         </div>
 
         <div className={classes.Details}>
-            <div>2 Beds</div>
-            <div>2 Baths</div>
-            <div>1 Garage</div>
-            <div>550 Sq Ft</div>
+
+            <div className={classes.DetailsInfo}>
+                <ReactSVG src={bed} className={classes.Icon}/>
+                <div>2 Beds</div>
+            </div>
+
+            <div className={classes.DetailsInfo}>
+                <ReactSVG src={bath} className={classes.Icon}/>
+                <div>2 Baths</div>
+            </div>
+
+            <div className={classes.DetailsInfo}>
+                <ReactSVG src={garage} className={classes.Icon}/>
+                <div>1 Garage</div>
+            </div>
+
+            <div className={classes.DetailsInfo}>
+                <ReactSVG src={home} className={classes.Icon}/>
+                <div>550 Sq Ft</div>
+            </div>
+       
         </div>
 
         <div className={classes.Adress}>
-            5721 avenue Saint-André, Montréal QC, H2J 4G3
+            <ReactSVG src={location} className={classes.Icon}/>
+            <div>5721 avenue Saint-André, Montréal QC, H2J 4G3</div>
         </div>
 
     </section>
