a12d957c4bb831c2abaad3938f706bebdf7f5c3f
Anja <rasoloanja@gmail.com>
2019-04-29 14:41:49 -0400

    Styled Top Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,9 +15,48 @@ import google from '../../assets/icon/SVG/google-plus.svg';
 const navbar = () => {
   return (
     <section className={classes.NavbarContainer}>
-        <section className = {classes.Contact}>
-                Contact me
-        </section>
+
+        {/* START NAV TOP*/}
+            <section className = {classes.NavTop}>
+
+            {/* ----NAV TOP CONTACT---- */}
+            <div className={classes.Contact}>
+                <div className={classes.NavTopItem}>
+                    <ReactSVG src={phone} className={classes.Icon}/>
+                    <span className={classes.NavTopTextContact}>+1 514 123 1234</span>
+                </div>
+                <div className={classes.NavTopItem}>
+                    <ReactSVG src={mail} className={classes.Icon}/>
+                    <span className={classes.NavTopText}>trimmo@myemail.com</span>
+                </div>
+            </div>
+
+        {/* ----NAV TOP USER---- */}
+            <section className={classes.User}>
+                <div className={classes.NavTopItem}>
+                    <ReactSVG src={user} className={classes.Icon}/>
+                    <span className={classes.NavTopText}>sign in</span>
+                </div>
+
+                <div className={classes.NavTopItem}>
+                    <ReactSVG src={pencil} className={classes.Icon}/>
+                    <span className={classes.NavTopText}>sign up</span>
+                </div>
+            </section>
+
+        {/* ----NAV TOP SOCIAL MEDIA---- */}
+            <section className={classes.SocialMedia}>
+                <ReactSVG src={facebook} className={classes.SocialIcon}/>
+                <ReactSVG src={twitter} className={classes.SocialIcon}/>
+                <ReactSVG src={linkedin} className={classes.SocialIcon}/>
+                <ReactSVG src={google} className={classes.SocialIcon}/>
+            </section>
+    </section>
+    {/* ----END NAV TOP---- */}
+
+
+                
+        
         <nav className={classes.Nav}>
             <li>
                 <NavLink  to='/' className={classes.Link}>
@@ -31,6 +70,7 @@ const navbar = () => {
                 </NavLink >
             </li>
         </nav>
+
     </section>
   )
 }
