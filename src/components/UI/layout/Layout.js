5495fa46fda2920ddd43affb4730e31795393d37
Anja <rasoloanja@gmail.com>
2019-04-29 12:26:01 -0400

    Added layout

---
+++ src/components/UI/layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Layout.css';
+
+const layout = (props) => {
+  return (
+    <div className={classes.Layout}>
+      {props.children}
+    </div>
+  )
+}
+
+
+export default layout;
+
