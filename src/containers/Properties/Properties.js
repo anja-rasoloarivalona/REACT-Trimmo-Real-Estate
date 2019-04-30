15b438ee9f74797e261906510cae44a2cc0e354f
Anja <rasoloanja@gmail.com>
2019-04-30 18:23:14 -0400

    Added title to properties

--- src/containers/Properties/Properties.js
+++ src/containers/Properties/Properties.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import classes from './Properties.css';
 import Form from './Form/PropertiesForm';
+import PropertiesList from '../../components/PropertiesList/PropertiesList';
 
  class Properties extends Component {
   render() {
     return (
-      <section className={classes.PropertiesContainer}>
+      <section className={classes.Properties}>
           <Form />
+          <section className={classes.PropertiesContainer}>
+              <div className={classes.PropertiesTitleContainer}>
+                    <div className={classes.PropertiesTitle}>
+                        <div>PROPERTIES</div>
+                        <div>FOR YOU</div>
+                    </div>
+                    <div className={classes.PropertiesTitleLine}>
+                        <hr/>
+                        <hr/>
+                    </div>
+              </div>
+              <PropertiesList />
+            </section>
+          
       </section>
     )
   }
