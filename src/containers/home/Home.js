f48c14bc338edfd3280c04b6ca210f5ec5d869d5
Anja <rasoloanja@gmail.com>
2019-04-29 23:06:37 -0400

    Added add info

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -44,10 +44,17 @@ class Home extends Component {
         style={{backgroundImage: "linear-gradient(110deg, rgba(198, 153, 99,1) 0%, rgba(198, 153, 99,1) 46%, white 46%, white 47%,  rgba(16, 29, 44,1) 47%)",
                 backgroundPosition: "center",
                 backgroundSize: "cover"}}>
+
           <div className={classes.AddInfo}>
+            <div>FIND YOUR</div>
+            <div>DREAM HOME</div>
           </div>
+
           <div className={classes.AddInfo}>
+            <div>WE ARE READY TO RECEIVE YOUR CALL</div>
+            <div>+1 514 123 1234</div>
           </div>
+
         </section>
 
         <section className={classes.Properties}>
