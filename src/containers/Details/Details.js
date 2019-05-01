48fc5d1da2f0981e10a85bd71bae3fa187956192
Anja <rasoloanja@gmail.com>
2019-05-01 18:19:37 -0400

    Init Property Details Header

--- src/containers/Details/Details.js
+++ src/containers/Details/Details.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import classes from './Details.css';
 import Property from '../../components/PropertiesList/Property/Property';
+import Form from './DetailsForm/DetailsForm';
+import bg from '../../assets/img/bg.jpg';
 
 
 
@@ -12,20 +14,20 @@ class Details extends Component {
    
     return (
       <div className={classes.Details}>
+
+          <section className={classes.Header}
+                    style={{backgroundImage: "url(" + bg + ")",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover"}}
+          >
+              <div className={classes.HeaderTitle}>
+                  PROPERTIES DETAILS
+              </div>
+              <Form />
+          </section>
     
                       
-      <Property 
-      
-                key={this.props.requestedProduct.id}
-                price ={this.props.requestedProduct.price}
-                title ={this.props.requestedProduct.title}
-                numbBeds={this.props.requestedProduct.numbBeds}
-                numbBaths={this.props.requestedProduct.numbBaths}
-                numbGarage={this.props.requestedProduct.numbGarage}
-                surface={this.props.requestedProduct.surface}
-                id={this.props.requestedProduct.id}
-                fullAdress={this.props.requestedProduct.fullAdress}
-        />
+  
                 
       </div>
 
@@ -52,5 +54,18 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps)(Details);
+/*
+<Property 
+      
+key={this.props.requestedProduct.id}
+price ={this.props.requestedProduct.price}
+title ={this.props.requestedProduct.title}
+numbBeds={this.props.requestedProduct.numbBeds}
+numbBaths={this.props.requestedProduct.numbBaths}
+numbGarage={this.props.requestedProduct.numbGarage}
+surface={this.props.requestedProduct.surface}
+id={this.props.requestedProduct.id}
+fullAdress={this.props.requestedProduct.fullAdress}
+/>*/
 
 
