39f650b9ecd69ab62dfea2284cb542bf3e313e25
Anja <rasoloanja@gmail.com>
2019-05-06 20:38:37 -0400

    fixed datat

--- src/data.js
+++ src/data.js
@@ -30,20 +30,18 @@ export const products = [
 
     {
         id:2,
-        lat: 45.538207,
-        lng: -73.637691,
+        lat: 45.505560,
+        lng: -73.570670,
         title: "Awesome appartement",
         infoWindow: "Awesome appartement",
         mainImg: "/img/properties/property02/img01.jpg",
         adress: {
-            fullAdress: "5478 rue Saint-André, Montréal QC, H2J 987",
-            streetNumber: 54,
-            streetName: "",
+            fullAdress: "505 Boulevard de Maisonneuve Ouest, Montréal QC, H3A 3C2",
+            streetNumber: 505,
+            streetName: "Boulevard de Maisonneuve Ouest",
             city: "Montréal",
             province: "QC",
-            postalCode: "ioiorebber"
-        },
-        
+            postalCode: "H3A 3C2"},  
         info: {
             price: "4 325 000",
             surface: 350,
@@ -53,91 +51,114 @@ export const products = [
             numbKitchen: 1,
             numbACrooms: 3,
             sauna: "yes",
-            jacuzzi: "yes", 
-        }           
+            jacuzzi: "yes"}           
     },
 
     {
         id:3,
-        lat: 45.53820,
-        lng: -73.537691,
-        fullAdress: "5478 rue Saint-André, Montréal QC, H2J 987",
+        lat: 45.507040,
+        lng: -73.565870,
         title: "Rooftop Appartment",
         infoWindow: "Rooftop Appartment",
-        price: "6 400 000",
-        surface: 550,
-        numbBeds: 4,
-        numbBaths: 2,
-        numbGarage: 2,
-        numBkitchen: 1,   
-        spa: "yes",
-        balcony: "yes",
-        fitnessCenter: "yes",
-        mainImg: "/img/properties/property03/img01.jpg"
-
-        
-        
+        mainImg: "/img/properties/property03/img01.jpg",
+        adress: {
+            fullAdress: "1100 Rue Jeanne-Mance, Montréal QC, H2Z 1L7",
+            streetNumber: 1100,
+            streetName: "Rue Jeanne-Mance",
+            city: "Montréal",
+            province: "QC",
+            postalCode: "H2Z 1L7"},
+        info: {
+            price: "6 400 000",
+            surface: 550,
+            numbBeds: 4,
+            numbBaths: 2,
+            numbGarage: 2,
+            numBkitchen: 1,   
+            spa: "yes",
+            balcony: "yes",
+            fitnessCenter: "yes"},            
     },
 
     {
         id:4,
-        lat: 45.47820,
-        lng: -73.367691,
-        fullAdress: "5478 rue Saint-André, Montréal QC, H2J 987",
+        lat: 45.487640,
+        lng: -73.596330,
         title: "Beautiful Condo",
         infoWindow: "Beautiful Condo",
-        price: "13 470 000",
-        surface: 450,
-        numbBeds: 5,
-        numbBaths: 3,
-        numbGarage: 2,
-        numBkitchen: 2,
-        numbACrooms: 7,
-        swimmingPool: "yes",
-        partyRooms: "yes", 
-        balcony: "yes",
-        pool: "yes",
-        mainImg: "/img/properties/property04/img01.jpg"
-        
-        
-        
+        mainImg: "/img/properties/property04/img01.jpg",
+        adress: {
+            fullAdress: "400 Avenue Clarke, Montréal QC, H3Z 2E7",
+            streetNumber: 400,
+            streetName: "Avenue Clarke",
+            city: "Montréal",
+            province: "QC",
+            postalCode: "H3Z 2E7"},
+        info: {
+            price: "13 470 000",
+            surface: 450,
+            numbBeds: 5,
+            numbBaths: 3,
+            numbGarage: 2,
+            numBkitchen: 2,
+            numbACrooms: 7,
+            swimmingPool: "yes",
+            partyRooms: "yes", 
+            balcony: "yes",
+            pool: "yes"}      
     },
+
     {
         id:5,
-        lat: 45.43820,
-        lng: -73.337691,
-        fullAdress: "5478 rue Saint-André, Montréal QC, H2J 987",
+        lat: 45.566965,
+        lng: -73.570833,
         title: "Modern Family House",
         infoWindow: "Modern Family House",
-        price: "17 890 000",
-        surface: 670,
-        numbBeds: 2,
-        numbBaths: 2,
-        numbGarage: 2,
-        numBkitchen: 2,
-        spa: "yes",
-        balcony: "yes",
-        fitnessCenter: "yes",
-        pool: "yes",
-        mainImg: "/img/properties/property05/img01.jpg"       
+        mainImg: "/img/properties/property05/img01.jpg",
+        adress: {
+            fullAdress: "4640 Rue de Bellechasse, Montréal QC, H1T 1Z3",
+            streetNumber: 4640,
+            streetName: "Rue de Bellechasse",
+            city: "Montréal",
+            province: "QC",
+            postalCode: " H1T 1Z3",
+        },
+        info: {
+            price: "17 890 000",
+            surface: 670,
+            numbBeds: 2,
+            numbBaths: 2,
+            numbGarage: 2,
+            numBkitchen: 2,
+            spa: "yes",
+            balcony: "yes",
+            fitnessCenter: "yes",
+            pool: "yes"}       
     },
 
     {
         id:6,
         lat: 45.53820,
         lng: -73.737691,
-        fullAdress: "2340 Avenue Bromond, Montreal QC, H2D P3T",
         title: "Awesome Villa",
         infoWindow: "Awesome Villa",
-        price: "21 500 000",
-        surface: 950,
-        numbBeds: 6,
-        numbBaths: 3,
-        numbGarage: 3,
-        numBkitchen: 2,
-        swimmingPool: "yes",
-        partyRooms: "yes", 
-        balcony: "yes",
-        mainImg: "/img/properties/property06/img01.jpg"  
+        mainImg: "/img/properties/property06/img01.jpg",
+        adress: {
+            fullAdress: "2134 Boulevard Gouin Est, Montreal QC, H2B 1X2",
+            streetNumber: 2134,
+            streetName: "Boulevard Gouin Est",
+            city: "Montréal",
+            province: "QC",
+            postalCode: " H2B 1X2"},
+        info : {
+            price: "21 500 000",
+            surface: 950,
+            numbBeds: 6,
+            numbBaths: 3,
+            numbGarage: 3,
+            numBkitchen: 2,
+            swimmingPool: "yes",
+            partyRooms: "yes", 
+            balcony: "yes"}     
     }
 ];
