da5d6cc8176e100b1e781b096fd4252bba2d939c
Anja <rasoloanja@gmail.com>
2019-05-30 15:18:30 -0400

    PropertiesList slice method added for home page

--- src/containers/home/properties/Properties.js
+++ src/containers/home/properties/Properties.js
@@ -1,27 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react'
 import classes from './Properties.css';
-import PropertiesList from '../../../components/PropertiesList/PropertiesList';
 import Title from '../../../components/UI/Title/Title';
+import { connect } from 'react-redux';
+import Property from '../../../components/PropertiesList/Property/Property';
 
-const properties = () => {
-    return (
-        <section className={classes.Properties}>
-            <div className={classes.PropertiesIntro}>
 
-                <Title title1= "AWESOME" 
-                        title2="PROPERTIES"/>
-                        
-                <p className={classes.PropertiesIntroPara}>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum totam et dolores voluptatem porro tempore temporibus ducimus
-                </p>
-            </div>
-            
-           <PropertiesList />
-            <div className={classes.PropertiesCTA}>
-                <button>MORE PROPERTIES</button>
-            </div>
-        </section>
-    )
+
+class Properties extends Component {
+    render() {
+
+        let properties = this.props.propertiesList.slice(0, 6);
+
+    
+        return (
+            <section className={classes.Properties}>
+                <div className={classes.PropertiesIntro}>
+    
+                    <Title title1= "AWESOME" 
+                            title2="PROPERTIES"/>
+                            
+                    <p className={classes.PropertiesIntroPara}>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum totam et dolores voluptatem porro tempore temporibus ducimus
+                    </p>
+                </div>
+                
+               <div className={classes.PropertiesList}>
+                    {properties.map(item => {
+                        return <Property key={item.id}
+                                price ={item.info.price}
+                                title ={item.title}
+                                numbBeds={item.info.beds}
+                                numbBaths={item.info.baths}
+                                numbGarage={item.info.garage}
+                                surface={item.info.surface}
+                                id={item.id}
+                                fullAddress={item.address.fullAddress}
+                                requestedDetail={() => this.props.onRequestedId(item.id)}/>}       
+                        )}
+               </div>
+    
+    
+                <div className={classes.PropertiesCTA}>
+                    <button>MORE PROPERTIES</button>
+                </div>
+            </section>
+        )
+        
+    }
 }
 
-export default properties;
+
+
+
+const mapStateToProps = state => {
+    return  {
+        propertiesList: state.products,
+    }
+}
+
+export default connect(mapStateToProps)(Properties);
